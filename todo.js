431556a7944b3943fe418e486419e88da17c94e3
Yaswanthkumar6 <yaswanthkumarsilla@gmail.com>
2026-02-06 19:47:05 +0530

    todo.js

--- todo.js
+++ todo.js
@@ -1,9 +1,7 @@
-/* ===== ROUTE PROTECTION ===== */
 if (localStorage.getItem("isLoggedIn") !== "true") {
   window.location.href = "index.html";
 }
 
-/* ===== LOGOUT FUNCTIONALITY ===== */
 const logoutBtn = document.getElementById("logoutBtn");
 
 logoutBtn.addEventListener("click", () => {
@@ -11,13 +9,15 @@ logoutBtn.addEventListener("click", () => {
   window.location.href = "index.html";
 });
 
-/* ===== TODO LOGIC ===== */
 const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
 
 let editMode = false;
 let taskBeingEdited = null;
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+loadTasks();
 
 addBtn.addEventListener("click", addOrEdit);
 
@@ -29,45 +29,72 @@ function addOrEdit() {
   }
 
   if (editMode) {
+    const index = tasks.indexOf(taskBeingEdited.textContent);
+    tasks[index] = text;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
     taskBeingEdited.textContent = text;
     addBtn.textContent = "Add";
     editMode = false;
     taskBeingEdited = null;
   } else {
-    const li = document.createElement("li");
+    tasks.push(text);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 
-    li.innerHTML = `
-      <div class="icons">
-        <input type="checkbox" class="delete">
-      </div>
-      <span class="task-text">${text}</span>
-      <div class="icons">
-        <i class="fa-solid fa-pen-to-square edit" title="Edit"></i>
-      </div>
-    `;
-
-    const editIcon = li.querySelector(".edit");
-    const deleteCheckbox = li.querySelector(".delete");
-    const taskSpan = li.querySelector(".task-text");
-
-    editIcon.addEventListener("click", () => {
-      taskInput.value = taskSpan.textContent;
-      taskInput.focus();
-      addBtn.textContent = "Edit";
-      editMode = true;
-      taskBeingEdited = taskSpan;
-    });
-
-    deleteCheckbox.addEventListener("change", () => {
-      if (deleteCheckbox.checked) {
-        setTimeout(() => {
-          li.remove();
-        }, 1000);
-      }
-    });
-
+    const li = createTask(text);
     taskList.appendChild(li);
   }
 
   taskInput.value = "";
+}
+
+function loadTasks() {
+  taskList.innerHTML = "";
+  tasks.forEach(task => {
+    taskList.appendChild(createTask(task));
+  });
+}
+
+function createTask(text) {
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+    <div class="icons">
+      <input type="checkbox" class="delete">
+    </div>
+    <span class="task-text">${text}</span>
+    <div class="icons">
+      <i class="fa-solid fa-pen-to-square edit"></i>
+    </div>
+  `;
+
+  const editIcon = li.querySelector(".edit");
+  const deleteCheckbox = li.querySelector(".delete");
+  const taskSpan = li.querySelector(".task-text");
+
+  editIcon.addEventListener("click", () => {
+    taskInput.value = taskSpan.textContent;
+    taskInput.focus();
+    addBtn.textContent = "Edit";
+    editMode = true;
+    taskBeingEdited = taskSpan;
+  });
+
+  deleteCheckbox.addEventListener("change", function () {
+  if (deleteCheckbox.checked) {
+
+    for (var i = 0; i < tasks.length; i++) {
+      if (tasks[i] === taskSpan.textContent) {
+        tasks[i] = "";
+        break;
+      }
+    }
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    li.remove();
+  }
+});
+
+
+  return li;
 }
